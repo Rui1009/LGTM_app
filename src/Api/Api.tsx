d94582c7552688ff107f2191684bfdc20235aac1
sonken625 <1113146c@gmail.com>
2020-01-01 21:20:30 +0900

    fix the bug of post image

--- src/Api/Api.tsx
+++ src/Api/Api.tsx
@@ -1,19 +1,5 @@
 import axios from "axios";
 
-const dataURLtoFile = (dataurl:string, filename:string) => {
-    const arr = dataurl.split(',');
-    const mime = arr[0].match(/:(.*?);/)![1]
-    const bstr = atob(arr[1])
-    let n = bstr.length
-    const u8arr = new Uint8Array(n)
-    while (n) {
-        u8arr[n] = bstr.charCodeAt(n)
-        n -= 1 // to make eslint happy
-    }
-    return new File([u8arr], filename, { type: mime })
-}
-
-
 export class Api {
     static async get(url: string) {
         return axios.get(url)
@@ -22,15 +8,20 @@ export class Api {
         return axios.put(url)
     }
 
-
-    static async postMultiPart(url:string,data:string){
-        const file = dataURLtoFile(data,"image.png")
-        const blobData = new FormData()
-        blobData.append('img', file, file.name)
-        // now upload
-        const config = {
-            headers: { 'Content-Type': 'multipart/form-data' }
-        }
-        return axios.post(url, data, config)
+    static async postMultiPart(url:string,dataUri:string){
+        return fetch(dataUri)
+          .then(res => res.blob())
+          .then(blob => {
+              const fd = new FormData()
+              fd.append('image', blob, 'filename.png')
+              axios(
+                {
+                    method: 'post',
+                    url: url,
+                    data: fd,
+                    headers: {'Content-Type': 'multipart/form-data' }
+                },
+              )
+          })
     }
 }
