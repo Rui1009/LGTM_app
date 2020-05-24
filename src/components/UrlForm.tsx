ccd52bc790acbef248f870f0e30136315430c41d
Rui <3116049c@gmail.com>
2020-05-24 10:17:02 +0900

    fix how to use description

--- src/components/UrlForm.tsx
+++ src/components/UrlForm.tsx
@@ -14,7 +14,7 @@ const UrlForm = () => {
 
     return (
             <div style={{margin: "10px 0"}}>
-                <Typography style={{padding:3}}>使い方：Googleで画像検索し、好きな画像のサムネイルをクリックして「画像アドレスをコピー」を押して以下のフォームにペーストし作成を押してください。</Typography>
+                <Typography style={{padding:3}}>使い方：Googleで画像検索するか、下の「検索から作成」から検索し、好きな画像のサムネイルをクリックして「画像アドレスをコピー」を押して以下のフォームにペーストし作成を押してください。</Typography>
                 <Grid container xs={12}>
                     <Grid item xs={6}>
                         <SendForm onSubmit={() => {
